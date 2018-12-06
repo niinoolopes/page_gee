bf0a4e52dba532891b3d3798c4a645b35789cd64
unknown <niinoolopes0@gmail.com>
2018-12-06 14:45:30 -0200

    updates

--- app/scripts/src/modal.js
+++ app/scripts/src/modal.js
@@ -1,14 +1,20 @@
 
-const blocoClick = document.querySelectorAll('.palestrante-bloco'); 
-
-
-blocoClick.forEach((item, index) => {
+const elementoAbreModal = document.querySelectorAll('[data-modal]');
+elementoAbreModal.forEach((item, index) => {
     item.addEventListener('click', (e) => {
-        if( e.target.className == 'palestrante-foto' || e.target.className == 'palestrante-nome' || e.target.className == 'modal-btn' || e.target.className == 'modal-container ativo' ){
-            abrirModal(index)
+        e.preventDefault();
+         document.getElementById(e.target.dataset.modal).classList.add('ativo');
+    })
+});
+var containerFechaModal = document.querySelectorAll('.modal-container');
+containerFechaModal.forEach((item, index) => {
+    item.addEventListener('click', (e) => {
+        if ( e.target.className == 'modal-btn' || e.target.className == 'modal-container ativo') {
+            document.querySelector('.modal-container.ativo').classList.remove('ativo');
         }
     })
-}); 
-function abrirModal(i) {
-    document.querySelectorAll('.modal-container')[i].classList.toggle('ativo') 
-}
+});
+
+
+
+
