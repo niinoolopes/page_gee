c34c7b83dfef03b7ead7e12e69223d4acef67698
niinoolopes <niinoolopes0@gmail.com>
2019-02-05 20:55:59 -0200

    Atualiza GULPFILE com resultados minificados

--- gulpfile.js
+++ gulpfile.js
@@ -1,146 +1,79 @@
-/* Variaveis */
-var gulp = require('gulp'); 
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload; 
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');  
-var autoprefixer = require('gulp-autoprefixer');
-var image = require('gulp-image');
+const gulp            = require('gulp');
+const browserSync     = require('browser-sync').create();
+const concat          = require('gulp-concat');
+const sass            = require('gulp-sass');
+const autoprefixer    = require('gulp-autoprefixer');
+const babel           = require('gulp-babel');
+const uglify          = require('gulp-uglify');
 
-
-/* Caminhos */
 const app = 'app';
 const structure = {
-    allFile_html: '**/*.html',
-    allFile_php: '**/*.php',
+    allFile_html: '**/*.html', 
 }
 const css = {
     name: 'estilo.css',
-    past: 'styles', 
-    src:  'scss',
-    allFile: '**/*.scss',
-    file_Grid: 'layout/sass.grid.scss',
+    name_min: 'estilo-min.css',
+    past: 'styles',
+    src: 'scss',
+    allFile: '**/*.scss', 
 }
 const javascript = {
     name: 'main.js',
+    name_min: 'main-min.js',
     past: 'scripts',
     src: 'src',
     allFile: '**/*.js',
 }
-const images = {
-    name: '',
-    past: 'images',
-    src: 'originais',
-    allFile: '**/*',
-}
 
-
-/*-TASK - SERVE -*/
 gulp.task('serve', function () {
     browserSync.init({
         server: {
             baseDir: app,
         }
-    }); 
-    console.log("SERVE - OK!");  
+    });
+    console.log("serve");
 });
 
-
-/*-TASK - STYLES - GRID -*/
-/*
-gulp.task('styles-grid', function(){
-    gulp.src( app +'/'+ css.past +'/'+ css.src + '/layout/sass-grid.scss') 
-    .pipe( sass() )
-    .pipe(concat('grid.scss'))
-    .pipe(gulp.dest( app +'/'+ css.past +'/'+ css.src + '/layout/'))
-    console.log("GRID OK!"); 
-});
-*/
-
-
-/*-TASK - STYLES - MINI -*/
-gulp.task('styles-mini',  function() {  
-    gulp.src( app +'/'+ css.past +'/'+ css.name)
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe( sass({
-        outputStyle: 'compressed'
-    }) )  
-    .pipe(gulp.dest( app +'/'+ css.past ))
-    console.log("STYLES - MINI OK!"); 
+gulp.task('styles-min', function () {
+    gulp.src(app + '/' + css.past + '/' + css.name)
+        .pipe(concat(css.name_min))
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
+        .pipe(gulp.dest(app + '/' + css.past))
+    console.log("styles-min");
 });
 
-
-/*-TASK - STYLES -*/
-gulp.task('styles', function() {  
-    gulp.src( app +'/'+ css.past +'/'+ css.src +'/'+ 'init.scss' )
-    .pipe( sass() )
-    // .pipe(autoprefixer({
-    //     browsers: ['last 2 versions'],
-    //     cascade: false
-    // }))
-    .pipe(concat( css.name ))
-    .pipe(gulp.dest( app +'/'+ css.past ))
-    .pipe(browserSync.stream());
-    console.log("STYLES OK!"); 
+gulp.task('styles', function () {
+    gulp.src(app + '/' + css.past + '/' + css.src + '/' + 'init.scss')
+        .pipe(concat(css.name))
+        .pipe(sass())
+        .pipe(gulp.dest(app + '/' + css.past))
+    console.log("styles");
 });
 
-
-/*-TASK - SCRIPTS -*/
-gulp.task('scripts',  function() {
-    gulp.src( app +'/'+javascript.past +'/'+ javascript.src + javascript.allFile) 
-    .pipe(concat( javascript.name ))
-    .pipe(gulp.dest( app +'/'+javascript.past ))
-    .pipe(browserSync.stream());
-    console.log("SCRIPTS OK!"); 
+gulp.task('scripts-min', function () {
+    gulp.src(app + '/' + javascript.past + '/' + javascript.name)
+        .pipe(concat(javascript.name_min))
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(app + '/' + javascript.past))
+    console.log("scripts-min");
 });
 
-
-/*-TASK - IMAGES -*/
-gulp.task('images',  function() {
-    gulp.src( app +'/'+ images.past + images.src + images.allFile)
-    .pipe(image({ optimizationLevel: 5, progressive: true, interlaced: true }) )
-    .pipe(gulp.dest( app +'/'+ images.past ))
-    .pipe(browserSync.stream());
-    console.log("IMAGES OK!"); 
+gulp.task('scripts', function () {
+    gulp.src(app + '/' + javascript.past + '/' + javascript.src + javascript.allFile)
+        .pipe(concat(javascript.name))
+        .pipe(gulp.dest(app + '/' + javascript.past))
+    console.log("scripts");
 });
 
-
-/*-TASK - HTML -*/
-gulp.task('html', function(){ 
-    browserSync.stream();
-}); 
-
-
-/*-TASK DEFAULT-*/
 gulp.task('default', () => {
- 
-    // gulp.run('styles-grid');
-    gulp.run('styles');
-    gulp.run('scripts');
-    gulp.run('images'); 
-    gulp.run('serve')  
-    console.log("GULP START OK!"); 
- 
-    gulp.watch( app +'/'+ css.past +'/'+ css.src +'/'+ css.allFile ,['styles']);
-    gulp.watch( app +'/'+ javascript.past +'/'+ javascript.src +'/'+ javascript.allFile ,['scripts']);
-    gulp.watch( app +'/'+ javascript.past +'/'+ javascript.allFile ).on('change', reload);
-    // gulp.watch( app +'/'+ images.past +'/'+ images.src +'/'+ images.allFile ,['images']);
-    // gulp.watch( app +'/'+ structure.allFile_html ,['html']);
-    gulp.watch( app +'/'+ structure.allFile_html ).on('change', reload);
-    console.log("WACTH OK!"); 
-
+    gulp.run('styles', 'styles-min', 'scripts', 'scripts-min', 'serve');
+    console.log('default');
+    gulp.watch(app + '/' + css.past + '/' + css.src + '/' + css.allFile, ['styles', 'styles-min']).on('change', browserSync.reload);
+    gulp.watch(app + '/' + javascript.past + '/' + javascript.src + '/' + javascript.allFile, ['scripts', 'scripts-min']).on('change', browserSync.reload);
+    gulp.watch(app + '/' + structure.allFile_html).on('change', browserSync.reload);
+    console.log("wacth!");
 });
- 
-
-//-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-// TASK TESTES
-gulp.task('teste', function(){  
-    gulp.watch( app +'/'+ css.past +'/'+ css.src +'/'+ css.allFile ,['styles']);
-    gulp.watch( app +'/'+ javascript.past +'/'+ javascript.src +'/'+ javascript.allFile ,['scripts']);
-    gulp.watch( app +'/'+ images.past +'/'+ images.src +'/'+ images.allFile ,['images']);
-    gulp.watch( app +'/'+ structure.allFile_html ,['html']);
-}); 
-//-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
